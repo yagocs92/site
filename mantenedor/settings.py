f0f668bc29788dc03dd32003386402cb53579774
Yago Costa Silva <125832351+yagocs92@users.noreply.github.com>
2023-06-19 20:26:28 -0300

    Update settings.py

--- mantenedor/settings.py
+++ mantenedor/settings.py
@@ -87,12 +87,6 @@ WSGI_APPLICATION = 'mantenedor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 import dj_database_url
 import os
