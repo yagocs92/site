0ea8a078c7f5fa76cfa9c769f6754cfd93ef7e2d
Yago Costa Silva <125832351+yagocs92@users.noreply.github.com>
2023-06-19 18:58:34 -0300

    Update settings.py

--- mantenedor/settings.py
+++ mantenedor/settings.py
@@ -87,7 +87,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgres://gpjubvjkke:{1Lithium23}@recogitate-server.postgres.database.azure.com/postgres?sslmode=require',
+        default='postgres://gpjubvjkke:1Lithium23@recogitate-server.postgres.database.azure.com/postgres?sslmode=require',
         conn_max_age=600,
         conn_health_checks=True,
     )
