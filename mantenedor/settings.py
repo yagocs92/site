67529e80a1414bfcde1d66acf294116b5bdd3a81
Yago Costa Silva <125832351+yagocs92@users.noreply.github.com>
2023-06-19 21:02:43 -0300

    Update settings.py

--- mantenedor/settings.py
+++ mantenedor/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['recogitate.azurewebsites.net', "127.0.0.1"]
 
-CSRF_TRUSTED_ORIGINS = ['https://*.recogitate.azurewebsites.net' "https://*.127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ['https://*.recogitate.azurewebsites.net', "https://*.127.0.0.1"]
 
 
 
